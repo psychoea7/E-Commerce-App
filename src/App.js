a702fdb893b918a8ac60beb82ddc70fbb723cbba
psychoea7 <sohrabnasehi.sn@gmail.com>
2023-03-16 14:28:11 +0330

    migrated to redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
 import { Routes, Route } from "react-router-dom";
+
+import {
+  createUserDocumentFromAuth,
+  onAuthStateChangeListener,
+} from "./utils/firebase/firebase.utils";
 
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component.jsx";
 import Shop from "./routes/shop/shop.component.jsx";
 import Checkout from "./routes/checkout/checkout.component";
+import { setCurrentUser } from "./store/user/user.action";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangeListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      dispatch(setCurrentUser(user));
+    });
+    return unsubscribe;
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../contexts/cart.context";
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
+
+
 import {
   CartDropdownContainer,
   EmptyMessage,
@@ -11,7 +13,7 @@ import {
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
-
+ 
   const navigate = useNavigate();
 
   const goToCheckoutHandler = () => {
